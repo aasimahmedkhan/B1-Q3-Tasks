6634c7d35c1da3e685ce75df2e7258d2e7e4bfda
ASIM AHMED KHAN <62013475+aasimahmedkhan@users.noreply.github.com>
2020-05-10 17:29:57 +0500

    UpdatedCapped.sol

--- CappedToken.sol
+++ CappedToken.sol
@@ -15,7 +15,8 @@ contract MintAndCappedToken is IERC20{
     mapping (address => mapping (address => uint256)) private _allowances;
 
     //the amount of tokens in existence
-    uint256 private _totalSupply;
+    uint256 private  _totalSupply;
+    
 
     //owner
     address public owner;
@@ -23,21 +24,22 @@ contract MintAndCappedToken is IERC20{
     string public name;
     string public symbol;
     uint8 public decimals;
-    uint256 constant totalSupplyCap = 100000500; // Constant Value
+    uint256 public  initialSupply = 1000000;
+    
     
    
 
-    constructor () public {
+    constructor () public  {
         name = "Capped Token";
         symbol = "MCT";
-        decimals = 2;
+        decimals = 0;
         owner = msg.sender;
-        
+        _totalSupply = 1005000 * (10 ** uint256(decimals));// 5000 minted tokens only (Constant)
         
         //1 million tokens to be generated
         //1 * (10**18)  = 1;
+        _totalSupply = 1005000 * (10 ** uint256(decimals));// 5000 minted tokens can't be exceed from Capped Limit
         
-        _totalSupply = 1000000 * (10 ** uint256(decimals));
         
         //transfer total supply to owner
         _balances[owner] = _totalSupply;
@@ -157,13 +159,15 @@ contract MintAndCappedToken is IERC20{
      * - amount should be valid incremental value.
      */
      /*     Capped Tokens
-        Constant Value (totalSupplyCap) = 100000500 CappedLimit
+        Constant Value (_totalSupply = 1005000 CappedLimit)
      */
     function mint(uint256 amount) public onlyOwner returns(uint256){
-        require(amount > 0,"MCT: Invalid Amount. Minted amount should be greater than 0");
-        _totalSupply = _totalSupply.add(amount);
-        require(_totalSupply <= totalSupplyCap, "MCT: Minting Tokens should not be exceed from CappedLimit = 100000500");
-        _balances[owner] = _balances[owner].add(amount);
+        require(initialSupply > 0,"MCT: Invalid Amount. Minted amount should be greater than 0");
+        initialSupply = initialSupply.add(amount);
+        require(initialSupply <= _totalSupply, "MCT: Minting Tokens should not be exceed from CappedLimit = 1005000");
+        
+       // _balances[owner] = _balances[owner].add(initialSupply);
+        
         
     }
 
